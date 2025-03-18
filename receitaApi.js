aded07149aec077d9757f211d1cbdf48bf58c3c0
LAURA SOFIA LIMA NUNES DA SILVA <24122467@CFP127ALUNO.LOCAL>
2025-03-18 17:25:41 -0300

    atualizando css

--- receitaApi.js
+++ receitaApi.js
@@ -1,3 +1,8 @@
 'use strict'
 
-const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
+const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+
+
+document.getElementById('pesquisar').addEventListener('click', prencherTela)
+
+
